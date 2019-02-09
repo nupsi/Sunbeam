b5e00bec1ee2d11bef70b8c779e9a05e9b28f85f
Nupsi <nupsi4.thenoob@gmail.com>
2019-02-09 16:57:44 +0200

    Added abstract class for effect. Started creating ice effect.

---
+++ Sunbeam/Script/Effects/Effect.cs
@@ -0,0 +1,42 @@
+﻿using Godot;
+
+namespace Sunbeam.Effects
+{
+    public abstract class Effect : Area2D
+    {
+        protected Node m_target;
+        private string m_targetName = "Player";
+
+        public override void _Ready()
+        {
+            Connect("body_entered", this, "EnterArea");
+            Connect("body_exited", this, "ExitArea");
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            if(m_target != null)
+            {
+                ApplyEffect(delta);
+            }
+        }
+
+        protected virtual void EnterArea(object body)
+        {
+            if((body as Node)?.GetName() == m_targetName)
+            {
+                m_target = (Node)body;
+            }
+        }
+
+        protected virtual void ExitArea(object body)
+        {
+            if((body as Node)?.GetName() == m_targetName)
+            {
+                m_target = null;
+            }
+        }
+
+        protected abstract void ApplyEffect(float delta);
+    }
+}
---
+++ Sunbeam/Script/Effects/Ice.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace Sunbeam.Effects
+{
+    public class Ice : Effect
+    {
+        protected override void ApplyEffect(float delta)
+        {
+            GD.Print("Player under ice effect! " + (m_target as Player));
+        }
+    }
+}
