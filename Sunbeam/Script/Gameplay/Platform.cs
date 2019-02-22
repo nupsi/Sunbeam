3cc749913be6ccf71d15c84b7fb7e11404001e2e
Nupsi <nupsi4.thenoob@gmail.com>
2019-02-22 13:02:06 +0200

    Added pausing for platform async functions

--- Sunbeam/Script/Gameplay/Platform.cs
+++ Sunbeam/Script/Gameplay/Platform.cs
@@ -43,10 +43,11 @@ namespace Sunbeam
                 while(m_bar.Value < 100)
                 {
                     m_bar.Value++;
-                    GD.Print(m_bar.Value);
                     await Task.Delay(BreakTime * 10);
+                    while(Paused) await PauseDelay;
                 }
                 StartRespawnTimer();
+                return;
             });
         }
 
@@ -55,10 +56,15 @@ namespace Sunbeam
             Enabled(false);
             await Task.Run(async () =>
             {
-                await Task.Delay(RespawnTime * 1000);
+                for(int i = 0; i < RespawnTime * 10; i++)
+                {
+                    await Task.Delay(RespawnTime * 10);
+                    while(Paused) await PauseDelay;
+                }
                 m_bar.Value = 0;
                 m_triggered = false;
                 Enabled(true);
+                return;
             });
         }
 
@@ -67,5 +73,8 @@ namespace Sunbeam
             Visible = enabled;
             m_collision.Disabled = !enabled;
         }
+
+        private bool Paused => GetTree().Paused;
+        private Task PauseDelay => Task.Delay(10);
     }
 }
